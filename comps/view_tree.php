bca47f611092f7c297da87a15f937b10c374e7a2
w74 <wolfram.rong@gmail.com>
2016-05-27 21:44:23 -0500

    Introduced Parsedown

--- comps/view_tree.php
+++ comps/view_tree.php
@@ -2,7 +2,61 @@
 
 $parse = new Parsedown();
 $json = json_decode(file_get_contents("js/info.json"), true);
+$id_count = 0;
 
-echo $parse->text('Hello _World_!');
+echo("<div role='tablist' aria-multiselectable='true'>
+        <div class='root branch' id='" . get_id('b')
+          . "' aria-controls='" . get_id('c')
+          . "' tabindex='" . get_id('t')
+          . "' role='tab' aria-selected='true' aria-expanded='false'>"
+            . $parse->text($json['root'])
+        . "</div>
+        <ul class='tree root' id='" . get_id('c')
+          . "' aria-labelledby='" . get_id('a')
+          . "' role='tabpanel' aria-hidden='true'>"); 
+print_tree($json['tree']); 
+echo("</ul>"); 
+ 
+function print_tree($tree){ 
+  global $parse;
+  foreach ($tree as $value){ 
+    if(isset($value['tree'])){
+
+      echo("<li class='node' role='tablist' aria-multiselectable='true'>
+              <div class='content branch' id='" . get_id('b')
+                . "' aria-controls='" . get_id('c')
+                . "' tabindex='" . get_id('t')
+                . "' role='tab' aria-expanded='false'>"
+                  . $parse->text($value['content'])
+              . "</div>
+              <ul class='tree' id='" . get_id('c')
+                . "' aria-labelledby='" . get_id('a')
+                . "' role='tabpanel' aria-hidden='true'>"); 
+      print_tree($value['tree']); 
+      echo("</ul>"); 
+    } else { 
+      echo("<li class='node'>
+              <div class='content'>"
+                . $parse->text($value['content'])
+              . "</div>"); 
+    }
+    echo("</li>"); 
+  } 
+}
+
+function get_id($trigger){
+  global $id_count;
+  switch ($trigger) {
+    case 'a':
+      return "b" . $id_count;
+    case 'b':
+      $id_count += 1;
+      return "b" . $id_count;
+    case 'c':
+      return "c" . $id_count;
+    case 't':
+      return $id_count;
+  }
+}
 
 ?>
--- js/script.js
+++ js/script.js
@@ -12,64 +12,54 @@ $(document).ready(function(){
 		'-webkit-animation-play-state': 'running',
 		'-moz-animation-play-state': 'running'
 	});
-});
 
+	// Opens branch and sets it as active
+	$('.branch').on('click keyup', function(e){
+		console.log('a');
+		// checks if key is the Enter key
+		if(e.keyCode && e.keyCode !== 13){return;}
 
-/*
-	Event (click, non-active branch)
-  	-> opens branch and sets it as active
- */
-$('body').on('click keyup', '.branch > *:not(.active)', function(e){
-	if(e.keyCode && e.keyCode !== 13){
-		return;
-	}
-	$(this).addClass('active');
-	var tree = $(this).parent().next('.tree');
-	var nodes = tree.children('.node');
-	tree.velocity('slideDown', {
-		duration: 400 * nodes.length,
-		easing: 'easeOutCubic'
-	});
-	/*
-		Function anon
-			-> expand horizontal line then flash in the text after a slightly random delay
-		@param {Int} i -> index
-		@param {DOM Node} i -> element being referenced
-	 */
-	nodes.each(function(i, e){
-		$(e).children('.lead-in').velocity({
-			width: ['3em', 0]
-		}, {
-			duration: 500,
-			easing: 'easeInCubic',
-			delay: 400 + (i * 250),
-			complete: function(){
-				$(e).children('.content').velocity({
-					opacity: [1, 0]
+		var tree = $(this).next('.tree');
+		
+		// if branch is active, close itself and all subsequent branches
+		if($(this).hasClass('active')){
+			$(this).removeClass('active');
+			tree.velocity('slideUp', {
+				duration: 400,
+				easing: 'easeOutCubic',
+				complete: function(){
+					tree.find('.tree').velocity('slideUp');
+					tree.find('.branch.active').removeClass('active');
+					tree.find('.content').css('opacity', 0);
+					tree.find('.lead-in').css('width', 0);
+				}
+			});
+		} else {
+			// if branch isn't active, expand it and display line animation
+			$(this).addClass('active');
+			var nodes = tree.children('.node');
+			tree.velocity('slideDown', {
+				duration: 400 * nodes.length,
+				easing: 'easeOutCubic'
+			});
+			nodes.each(function(i, e){
+				$(e).children('.lead-in').velocity({
+					width: ['3em', 0]
 				}, {
 					duration: 500,
-					easing: 'flicker',
-					delay: Math.random() * 500
+					easing: 'easeInCubic',
+					delay: 400 + (i * 250),
+					complete: function(){
+						$(e).children('.content').velocity({
+							opacity: [1, 0]
+						}, {
+							duration: 500,
+							easing: 'flicker',
+							delay: Math.random() * 500
+						});
+					}
 				});
-			}
-		});
-	});
-});
-
-$('body').on('click keyup', '.branch > *.active', function(e){
-	if(e.keyCode && e.keyCode !== 13){
-		return;
-	}
-	$(this).removeClass('active');
-	var tree = $(this).parent().next('.tree');
-	tree.velocity('slideUp', {
-		duration: 400,
-		easing: 'easeOutCubic',
-		complete: function(){
-			tree.find('.tree').velocity('slideUp');
-			tree.find('p.active').removeClass('active');
-			tree.find('.content').css('opacity', 0);
-			tree.find('.lead-in').css('width', 0);
+			});
 		}
 	});
 });
